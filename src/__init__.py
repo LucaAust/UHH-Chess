70b4281a49208b1a672964c813bcca9378656ae8
LucaAust <luca@austelat.de>
2024-02-19 10:51:55 +0100

    update libs

--- src/__init__.py
+++ src/__init__.py
@@ -6,7 +6,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 from src.lib.astl_logger import AstlLogger
 from src.lib.sql import SQL
